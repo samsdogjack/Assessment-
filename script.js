79dddde87ebd5e53a8a79fde391072dd75011604
Shane Spillman <shane@Shanes-MacBook-Pro.local>
2022-06-14 20:58:56 -0400

    finished

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Obtain the img element, and assign it to the image variable
-let image 
+let image = document.querySelector('#pet')
+
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize 
+let changeSize = document.querySelector('#shrink-grow')
+
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
